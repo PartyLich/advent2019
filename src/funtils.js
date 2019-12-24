c09c9680c8455a536b2e54c085e2130e3f25333e
Arthur E. Jones <PartyLich@gmail.com>
2019-12-23 20:32:26 -0500

    feat: add pipe to funtils

--- src/funtils.js
+++ src/funtils.js
@@ -3,5 +3,7 @@ export const identity = (x) => x;
 // string -> number
 export const toDecimal = (str) => parseInt(str, 10);
 
+export const pipe = (...fns) => (x) => fns.reduce((acc, f) => f(acc), x);
+
 // number -> number
 export const sum = (a, b) => a + b;
