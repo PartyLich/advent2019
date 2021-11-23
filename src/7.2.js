a11b94b7ec93cce0ad9c872d0fd1772f65813f09
Arthur E. Jones <PartyLich@gmail.com>
2021-11-23 12:15:06 -0600

    feat: run amp loop with a list of phase settings

--- src/7.2.js
+++ src/7.2.js
@@ -3,6 +3,7 @@
 // part 2
 import { add, OPCODES, parseInstruction } from './5';
 import { transition } from './5.2';
+import { makeOpcodes } from './7';
 
 
 // returns an Iterable over computer state
@@ -43,3 +44,48 @@ export const computer = (opcodes) => ({ state: program = [], pc = 0 } = {}) => {
     },
   };
 };
+
+// run an amplifier loop with the provided phase settings, returning the last
+// output
+// []number -> []number -> number -> number
+export const trySettings = (program) => (phaseSettings) =>
+  (initialInput = 0) => {
+    const amps = Array(phaseSettings.length);
+    const phaseIter = phaseSettings[Symbol.iterator]();
+
+    // run amp loop to completion
+    let done = false;
+    let output = 0;
+    let lastOutput;
+    while (!done) {
+      for (const [i, amp] of amps.entries()) {
+        const inputList = [];
+        const nextPhase = phaseIter.next();
+        if (!nextPhase.done) inputList.push(nextPhase.value);
+        inputList.push(output);
+
+        const inputIter = inputList[Symbol.iterator]();
+        const stateIter = computer(makeOpcodes(
+            () => inputIter.next().value,
+            (value) => {
+              // side effect: set accumulator to output value
+              output = value;
+              lastOutput = value;
+            },
+        ))(amp ? amp : { state: program });
+
+        // run amp until output or HALT
+        output = null;
+        let ampState;
+        for (ampState of stateIter) {
+          if (output !== null) break;
+        }
+
+        (ampState === undefined)
+            ? done = true
+            : amps[i] = ampState;
+      }
+    }
+
+    return lastOutput;
+  };
--- src/7.2.test.js
+++ src/7.2.test.js
@@ -3,6 +3,7 @@ import test from 'tape';
 import { makeOpcodes } from './7';
 import {
   computer,
+  trySettings,
 } from './7.2';
 
 
@@ -49,6 +50,35 @@ test('7.2: computer()', (t) => {
     t.equal(actual.state[26], expected, msg);
     t.equal(actual.state[28], expected, msg);
     t.equal(output, expected, msg);
+  }
+  t.end();
+});
+
+test('7.2: trySettings()', (t) => {
+  const msg = 'returns final thruster signal after amplification';
+  {
+    const program = [
+      3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+      27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5,
+    ];
+    const phases = [9, 8, 7, 6, 5];
+
+    const expected = 139629729;
+    const actual = trySettings(program)(phases)();
+    t.equal(actual, expected, msg);
+  }
+  {
+    const program = [
+      3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55,
+      26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001,
+      55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0,
+      0, 0, 10,
+    ];
+    const phases = [9, 7, 8, 5, 6];
+
+    const expected = 18216;
+    const actual = trySettings(program)(phases)();
+    t.equal(actual, expected, msg);
   }
   t.end();
 });
