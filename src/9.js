79039cc099fea9962ef6415eb34ba11fc6cad456
Arthur E. Jones <PartyLich@gmail.com>
2021-11-23 19:11:25 -0600

    feat: add relative base offset opcode

--- src/9.js
+++ src/9.js
@@ -1,5 +1,9 @@
 // Advent of Code 2019
 // Day 9: Sensor Boost
+import {
+  OPCODES as PREV_OPCODES,
+} from './5.2';
+
 
 // type Operation {
 //  fn: Function,
@@ -20,4 +24,20 @@ export const Computer = (program = []) => ({
   mem: program,
   pc: 0,
   rb: 0,
+});
+
+// adjusts the relative base by the value of the offset parameter
+// number -> number
+export const rbOffset = (offset) => offset;
+
+// opcode map
+export const OPCODES = Object.assign(PREV_OPCODES, {
+  9: { fn: rbOffset, params: 1 },
+});
+
+// set up opcodes with the specified input and output fns
+// (ie supply the effectful computations)
+export const makeOpcodes = (inputFn, outputFn) => Object.assign(OPCODES, {
+  3: { fn: inputFn, params: 1 },
+  4: { fn: outputFn, params: 1 },
 });
