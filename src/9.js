f6dc034da7ac7bb0b454c14bb63d0aba93aa5d59
Arthur E. Jones <PartyLich@gmail.com>
2021-11-23 19:13:23 -0600

    feat: add relative mode argument support

--- src/9.js
+++ src/9.js
@@ -41,3 +41,25 @@ export const makeOpcodes = (inputFn, outputFn) => Object.assign(OPCODES, {
   3: { fn: inputFn, params: 1 },
   4: { fn: outputFn, params: 1 },
 });
+
+const POSITION = 0;
+const IMMEDIATE = 1;
+const RELATIVE = 2;
+
+// Computer -> []number -> (number, number) -> number
+export const loadArgs = ({ mem = [], rb = 0 } = {}) => (modes = []) =>
+  (arg, i) => {
+    // read modes from back to front
+    const mode = modes[modes.length - 1 - i] || 0;
+
+    switch (mode) {
+      case IMMEDIATE:
+        return arg;
+      case RELATIVE:
+        return mem[rb + arg] || 0;
+      case POSITION:
+      // intentional fallthrough
+      default:
+        return mem[arg] || 0;
+    }
+  };
