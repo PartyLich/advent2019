ae00680eee429a8b88f5993e59bd45c61ea23255
Arthur E. Jones <PartyLich@gmail.com>
2021-11-25 15:26:51 -0600

    feat: add latest iterable Intcode computer impl
    Now with 10% more generator functions!

---
+++ src/11/11.js
@@ -0,0 +1,61 @@
+// Advent of Code 2019
+// Day 11: Space Police
+import { parseInstruction, parseOpcode, parseMode } from '../5';
+import {
+  OPCODES,
+  makeOpcodes,
+  transition,
+} from '../9';
+
+
+// opcode constants
+const INPUT = 3;
+const OUTPUT = 4;
+
+// return iterator over [Computer, number | undefined]
+export const makeGen = function* ({ mem = [], pc = 0, rb = 0 } = {}) {
+  let state = {
+    mem: mem.slice() || [],
+    pc,
+    rb,
+  };
+  const outputBuf = [];
+  let input;
+  const inputBuf = [];
+
+  const opcodes = makeOpcodes(
+      // take input from head of queue
+      () => inputBuf.shift(),
+      // set output buffer
+      (value) => outputBuf.push(value),
+  );
+
+  while (true) {
+    let { opcode, modes } = parseInstruction(state.mem[state.pc]);
+    let op = opcodes[opcode];
+
+    while (op.fn) {
+      if (op === OPCODES[INPUT]) {
+        // yield same state until we get input (e.g. Block)
+        while (input === undefined) {
+          input = yield [state, undefined];
+        }
+        inputBuf.push(input);
+        input = undefined;
+      }
+
+      state = transition(state, op, modes);
+
+      if (op === OPCODES[OUTPUT]) {
+        // yield that output baby
+        input = yield [state, outputBuf.shift()];
+      }
+
+      opcode = parseOpcode(state.mem[state.pc]);
+      modes = parseMode(state.mem[state.pc]);
+      op = opcodes[opcode];
+    }
+
+    return [state, outputBuf.shift()];
+  }
+};
---
+++ src/11/11.test.js
@@ -0,0 +1,96 @@
+import test from 'tape';
+
+import { Computer } from '../9';
+import {
+  makeGen,
+} from './11';
+
+
+test('11: generator', (t) => {
+  {
+    const msg = 'produces a copy of itself as output';
+    const program = [
+      109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
+    ];
+    const computer = makeGen(Computer(program));
+    const actual = [];
+    for (const [_state, output] of computer) {
+      actual.push(output);
+    }
+
+    t.deepEqual(actual, program, msg);
+  }
+  {
+    const msg = 'produces a copy of itself as output';
+    const program = [
+      109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
+    ];
+    const computer = makeGen(Computer(program));
+    const actual = [...computer].map(([_state, output]) => output);
+    t.deepEqual(actual, program, msg);
+  }
+  {
+    const msg = 'program should output a 16 digit number';
+    const program = [
+      1102, 34915192, 34915192, 7, 4, 7, 99, 0,
+    ];
+    const expected = 16;
+    let actual;
+
+    const computer = makeGen(Computer(program));
+    for (const [_state, output] of computer) {
+      actual = `${ output }`;
+    }
+
+    t.equal(actual.length, expected, msg);
+  }
+  {
+    const msg = '2 == 2, so set idx 9 to 1';
+    const program = [
+      9, 1, 2108, 2, 2, 9, 4, 9, 99, 5,
+    ];
+    const expected = 1;
+    let actual;
+
+    const computer = makeGen(Computer(program));
+    for (const [_state, output] of computer) {
+      actual = output;
+    }
+    t.equal(actual, expected, msg);
+  }
+  {
+    const msg = '2 + 2 = 4';
+    const program = [
+      9, 1, 2101, 2, 2, 9, 4, 9, 99, 4,
+    ];
+    const expected = 4;
+    let actual;
+
+    const computer = makeGen(Computer(program));
+    for (const [_state, output] of computer) {
+      actual = output;
+    }
+    t.equal(actual, expected, msg);
+  }
+  {
+    const msg = 'input test. 3 + 3 = 6';
+    const program = [
+      9, 1, 3, 6, 2101, 3, -1, 11, 4, 11, 99, -2,
+    ];
+    const expected = 6;
+    let actual;
+    const computer = makeGen(Computer(program));
+
+    let next = computer.next();
+    t.equal(next.value[0].pc, 2, 'yields at first input instruction');
+
+    while (!next.done) {
+      actual = next.value[1];
+      // provide i = 5 - RB. RB should be 1 at this point, so
+      next = computer.next(4);
+    }
+
+    t.equal(actual, expected, msg);
+  }
+  t.end();
+});
