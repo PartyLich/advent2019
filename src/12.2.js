49fb0ba1c483e599edfa2f63e76080a075536a2d
Arthur E. Jones <PartyLich@gmail.com>
2021-11-29 15:57:02 -0600

    feat: find repeat intervals for initial body states

--- src/12.2.js
+++ src/12.2.js
@@ -1,6 +1,9 @@
 // Advent of Code 2019
 // Day 12: The N-Body Problem
 // part 2
+import { pipe } from './funtils';
+import { stepOnce } from './12';
+
 
 // array equality. Check nested objects by reference (although we only have
 // primitives in this specific usage)
@@ -15,3 +18,42 @@ export const eq = (arr1) => (arr2) => {
 
   return true;
 };
+
+// number -> bool
+const negative = (i) => i < 0;
+
+// map bodies to arrays of initial values
+const positions = (bodies) =>
+  [0, 1, 2]
+      .map((idx) => bodies.flatMap((body) => [body.pos[idx], body.vel[idx]]));
+
+// find repeat intervals for initial body states
+const getIntervals = (bodies) => {
+  const initPos = positions(bodies);
+  let intervals = Array(3).fill(-Infinity);
+  let step = 1;
+
+  while (intervals.some(negative)) {
+    bodies = stepOnce(bodies);
+    const curPos = positions(bodies);
+    intervals = intervals.map((interval, idx) => {
+      if (!negative(interval)) return interval;
+
+      return eq(initPos[idx])(curPos[idx])
+          ? step
+          : interval;
+    });
+
+    step += 1;
+  }
+
+  return intervals;
+};
+
+// find LCM of state repeat intervals for each dimension
+// I didnt come up with the necessary idea. After a bunch of time reading N-body
+// problem wiki pages and determining what I remember about differential
+// equations from over a decade ago (to whit: not enough to be usable), I gave
+// up and found this
+// [tip/explanation](https://www.reddit.com/r/adventofcode/comments/e9jxh2/comment/far9cgu/?utm_source=reddit&utm_medium=web2x&context=3)
+export const solve = pipe(getIntervals);
