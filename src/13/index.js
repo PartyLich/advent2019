2c27ef78c289c9dfa08d47ad05c7e33256ce83f3
Arthur E. Jones <PartyLich@gmail.com>
2021-12-04 18:58:30 -0600

    - export console drawing constants

--- src/13/index.js
+++ src/13/index.js
@@ -2,4 +2,9 @@ export { inpFilter } from '../3';
 export { inpMap } from '../5';
 export { solve } from './13';
 export { solve as solve2 } from './13.2';
-export { solve as visual } from './13.2.viz';
+export {
+  solve as visual,
+  SOFT_CLEAR,
+  SET_CURSOR,
+} from './13.2.viz';
+
