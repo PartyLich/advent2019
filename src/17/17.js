af941e6df1784820a02236c52a80175d68254c8d
Arthur E. Jones <PartyLich@gmail.com>
2021-12-07 12:09:38 -0600

    feat: add get2d fn
    Convenience function to index 1d array as though it was 2d

--- src/17/17.js
+++ src/17/17.js
@@ -1,2 +1,42 @@
 // Advent of Code 2019
 // Day 17: Set and Forget
+import { pipe } from '../funtils';
+
+import { Computer } from '../9';
+import { makeGen } from '../11/11';
+
+
+// retrieve camera output from an intcode program
+// Computer -> []number
+const getMap = (computer) => {
+  const bot = makeGen(computer);
+  const map = [];
+
+  for (const [, output] of bot) {
+    map.push(output);
+  }
+
+  return map;
+};
+
+// get array value with 2d index
+export const get2d = (width = 0) => (arr = []) => (row = 0) => (col = 0) =>
+  arr[(width * row) + col];
+
+// create a computer from the supplied program
+// []number -> Computer
+const makeComputer = pipe(
+    (input) => input.flat(),
+    Computer,
+);
+
+// What is the sum of the alignment parameters for the scaffold intersections?
+// []number -> number
+export const solve = pipe(
+    makeComputer,
+    getMap,
+    (map) => {
+      console.log(String.fromCharCode(...map));
+      return map;
+    },
+);
---
+++ src/17/17.test.js
@@ -0,0 +1,23 @@
+import test from 'tape';
+import {
+  get2d,
+} from './17';
+
+
+test('17: get2d()', (t) => {
+  {
+    const msg = 'returns value as though array was 2x2';
+    const arr = [1, 2, 3, 4];
+    const width = 2;
+
+    let expected = 1;
+    for (let r = 0; r < 2; r++) {
+      for (let c = 0; c < 2; c++) {
+        const actual = get2d(width)(arr)(r)(c);
+        t.equal(actual, expected, msg);
+        expected += 1;
+      }
+    }
+  }
+  t.end();
+});
