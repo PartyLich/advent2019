3ed3eefe2701ff30abbc0aa98388ba6890edf589
Arthur E. Jones <PartyLich@gmail.com>
2021-12-07 15:15:11 -0600

    refactor: extract getDimensions

--- src/17/17.js
+++ src/17/17.js
@@ -24,16 +24,24 @@ const getMap = (computer) => {
 export const get2d = (width = 0) => (arr = []) => (row = 0) => (col = 0) =>
   arr[(width * row) + col];
 
+// return the dimensions of this 2d map
+// []any -> { width: number, height: number }
+export const getDimensions = (map) => {
+  // NOTE: lines terminated by newline char
+  const width = map.indexOf(10) + 1;
+  // NOTE: bonus newline char at the end
+  const height = (map.length - 1) / width;
+  return { width, height };
+};
+
+
 // return the sum of the alignment parameters
 // for each scaffold intersection, its alignment parameter is the distance
 // between its left edge and the left edge of the view multiplied by the
 // distance between its top edge and the top edge of the view.
 // []number -> number
 export const sumAlignmentParameters = (map) => {
-  // NOTE: lines terminated by newline char
-  const width = map.indexOf(10) + 1;
-  // NOTE: bonus newline char at the end
-  const height = (map.length - 1) / width;
+  const { width, height } = getDimensions(map);
   const get = get2d(width)(map);
 
   let alignmentSum = 0;
