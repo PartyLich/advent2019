2bf7f19df9d3ab07feaec8124605a6988af4d79d
Arthur E. Jones <PartyLich@gmail.com>
2021-12-18 14:31:59 -0600

    feat: find list of neighbors and their status

--- src/24/24.2.js
+++ src/24/24.2.js
@@ -18,3 +18,86 @@ export const fmt = (input) => input.reduce(
     ))),
     new Set(),
 );
+
+// Set<bool> -> (number, number, number) -> bool
+const getTile = (map) => (r, c, z) => map.has(toKey(r, c, z));
+
+// return list of neighbors and their status
+// ((r, c, z) => bool) -> (r, c, z) -> [][bool, string]
+const getNeighbors = (getTile) => (r, c, z) => {
+  const top = [getTile(r - 1, c, z), toKey(r - 1, c, z)];
+  const right = [getTile(r, c + 1, z), toKey(r, c + 1, z)];
+  const bottom = [getTile(r + 1, c, z), toKey(r + 1, c, z)];
+  const left = [getTile(r, c - 1, z), toKey(r, c - 1, z)];
+  if (c === 2 && r == 1) {
+    return [
+      top,
+      right,
+      // 5 bottom
+      [getTile(0, 0, z + 1), toKey(0, 0, z + 1)],
+      [getTile(0, 1, z + 1), toKey(0, 1, z + 1)],
+      [getTile(0, 2, z + 1), toKey(0, 2, z + 1)],
+      [getTile(0, 3, z + 1), toKey(0, 3, z + 1)],
+      [getTile(0, 4, z + 1), toKey(0, 4, z + 1)],
+      left,
+    ];
+  }
+  if (c === 2 && r == 3) {
+    return [
+      // 5 top
+      [getTile(4, 0, z + 1), toKey(4, 0, z + 1)],
+      [getTile(4, 1, z + 1), toKey(4, 1, z + 1)],
+      [getTile(4, 2, z + 1), toKey(4, 2, z + 1)],
+      [getTile(4, 3, z + 1), toKey(4, 3, z + 1)],
+      [getTile(4, 4, z + 1), toKey(4, 4, z + 1)],
+      right,
+      bottom,
+      left,
+    ];
+  }
+  if (c === 1 && r == 2) {
+    return [
+      top,
+      // 5 rights
+      [getTile(0, 0, z + 1), toKey(0, 0, z + 1)],
+      [getTile(1, 0, z + 1), toKey(1, 0, z + 1)],
+      [getTile(2, 0, z + 1), toKey(2, 0, z + 1)],
+      [getTile(3, 0, z + 1), toKey(3, 0, z + 1)],
+      [getTile(4, 0, z + 1), toKey(4, 0, z + 1)],
+      bottom,
+      left,
+    ];
+  }
+  if (c === 3 && r == 2) {
+    return [
+      top,
+      right,
+      bottom,
+      // 5 lefts
+      [getTile(0, 4, z + 1), toKey(0, 4, z + 1)],
+      [getTile(1, 4, z + 1), toKey(1, 4, z + 1)],
+      [getTile(2, 4, z + 1), toKey(2, 4, z + 1)],
+      [getTile(3, 4, z + 1), toKey(3, 4, z + 1)],
+      [getTile(4, 4, z + 1), toKey(4, 4, z + 1)],
+    ];
+  }
+
+  return [
+    // top in next plane?
+    (r === 0)
+        ? [getTile(1, 2, z - 1), toKey(1, 2, z - 1)]
+        : top,
+    // right in next plane?
+    (c === 4)
+        ? [getTile(2, 3, z - 1), toKey(2, 3, z - 1)]
+        : right,
+    // bottom in next plane?
+    (r === 4)
+        ? [getTile(3, 2, z - 1), toKey(3, 2, z - 1)]
+        : bottom,
+    // left in next plane?
+    (c === 0)
+        ? [getTile(2, 1, z - 1), toKey(2, 1, z - 1)]
+        : left,
+  ];
+};
