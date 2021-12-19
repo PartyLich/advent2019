16f70e8d42b7fe6839e15312f862a1c613630766
Arthur E. Jones <PartyLich@gmail.com>
2021-12-18 20:02:52 -0600

    feat: add 23-1 solution

--- src/23/23.js
+++ src/23/23.js
@@ -23,3 +23,48 @@ const makeNetwork = (size) => (program) => Array(size)
       const comp = makeComputer(program);
       return { comp, next: comp.next() };
     });
+
+// run network until a packet is sent to address 255
+// []Computer -> number
+const runNetwork = (net) => {
+  // fill input q with network addresses
+  const inputQ = Array(net.length).fill(null)
+      .map((_, ip) => [ip]);
+
+  while (true) {
+    for (const [addr, { comp, next }] of net.entries()) {
+      if (!next.done) {
+        const [, dest] = next.value;
+        if (dest === undefined) {
+          // expecting input
+          // If the incoming packet queue is empty, provide -1
+          const input = inputQ[addr].length ? inputQ[addr].shift() : -1;
+          net[addr].next = comp.next(input);
+
+          continue;
+        }
+
+        // To send a packet to another computer, the NIC will use three output
+        // instructions that provide the destination address of the packet
+        // followed by its X and Y values.
+        const [, x] = comp.next().value;
+        const [, y] = comp.next().value;
+
+        if (dest === 255) {
+          return y;
+        }
+
+        inputQ[dest].push(x);
+        inputQ[dest].push(y);
+        net[addr].next = comp.next();
+      }
+    }
+  }
+};
+
+// What is the Y value of the first packet sent to address 255?
+// []number -> number
+export const solve = pipe(
+    makeNetwork(50),
+    runNetwork,
+);
