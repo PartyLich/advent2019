c11ae8afe4ca3a315995179ca6173b5090ba43a6
Arthur E. Jones <PartyLich@gmail.com>
2021-12-21 13:05:52 -0600

    feat: add 23-2 solution

--- src/23/23.js
+++ src/23/23.js
@@ -68,3 +68,78 @@ export const solve = pipe(
     makeNetwork(50),
     runNetwork,
 );
+
+//  What is the first Y value delivered by the NAT to the computer at address 0
+//  twice in a row?
+// []Computer -> number
+const part2 = (net) => {
+  // fill input q with network addresses
+  const inputQ = Array(net.length).fill(null)
+      .map((_, ip) => [ip]);
+  let nat = [];
+  let lastY;
+  let idleTime = 0;
+
+  while (true) {
+    let packetSent = false;
+    for (const [addr, { comp, next }] of net.entries()) {
+      if (!next.done) {
+        const [, dest] = next.value;
+        if (dest === undefined) {
+          // expecting input
+          // If the incoming packet queue is empty, provide -1
+          const input = inputQ[addr].length ? inputQ[addr].shift() : -1;
+          net[addr].next = comp.next(input);
+
+          continue;
+        }
+
+        // To send a packet to another computer, the NIC will use three output
+        // instructions that provide the destination address of the packet
+        // followed by its X and Y values.
+        const [, x] = comp.next().value;
+        const [, y] = comp.next().value;
+
+        if (dest === 255) {
+          // If a packet would be sent to address 255, the NAT receives it
+          // instead.
+          // Stores only the last packet it receives;
+          nat = [x, y];
+        } else {
+          inputQ[dest].push(x);
+          inputQ[dest].push(y);
+        }
+
+        packetSent = true;
+        net[addr].next = comp.next();
+      }
+    }
+
+    // If all computers have empty incoming packet queues and are continuously
+    // trying to receive packets without sending packets, the network is
+    // considered idle.
+    const inputWaiting = inputQ.some((q) => q.length);
+    idleTime = (packetSent || inputWaiting) ? 0 : idleTime + 1;
+
+    // Once the network is idle, the NAT sends only the last packet it received
+    // to address 0; this will cause the computers on the network to resume
+    // activity.
+    if (idleTime >= 3) {
+      const [x, y] = nat;
+      inputQ[0].push(x);
+      inputQ[0].push(y);
+
+      if (lastY === y) return y;
+
+      lastY = y;
+    }
+  }
+};
+
+//  What is the first Y value delivered by the NAT to the computer at address 0
+//  twice in a row?
+// []number -> number
+export const solve2 = pipe(
+    makeNetwork(50),
+    part2,
+);
